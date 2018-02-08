0234d9c1cc6e2743120a3e17d39c61cb428b209a
jshannon75 <jshannon@uga.edu>
2018-02-07 20:46:07 -0500

    Revised script and 100k data

--- taxi_data_sample_2018_02_01.R
+++ taxi_data_sample_2018_02_01.R
@@ -3,6 +3,8 @@ library(sf)
 library(lubridate)
 library(stringr)
 
+#Note: Download this file to your working folder before starting:
+#http://www.shannons.us/Files/2016_Yellow_Taxi_Trip_Data.csv
 taxi_data <- read_csv("2016_Yellow_Taxi_Trip_Data.csv") %>%
   filter(trip_distance>0 & fare_amount>0) %>%
   mutate(pickup_time=parse_date_time(tpep_pickup_datetime, '%m/%d/%y %I:%M:%S %p'),
@@ -17,37 +19,16 @@ taxi_data_sample<- taxi_data %>%
          pickup_latitude>40.6 & pickup_latitude<40.9 &
          dropoff_longitude> -74.1 & dropoff_longitude< -73.66 &
          dropoff_latitude>40.6 & dropoff_latitude<40.9) %>%
-  sample_n(200000) %>%
+  sample_n(100000) %>%
   mutate(recordIDnum=c(1:nrow(.)),
          recordID=paste("T",str_pad(recordIDnum, 6, pad = "0"),sep=""),
          tipfare_rat=tip_amount/fare_amount)
 
 taxi_data_select<- taxi_data_sample %>%
-  select(recordID,pickup_time,dropoff_time,pickup_longitude,pickup_latitude,dropoff_longitude,dropoff_latitude,tipfare_rat)
+  select(recordID,pickup_time,dropoff_time,pickup_longitude,pickup_latitude,dropoff_longitude,dropoff_latitude,tip_amount,fare_amount)
 
-write_csv(taxi_data_select,"taxi_data_subset_nyc_ratios_200k.csv")
+write_csv(taxi_data_select,"taxi_data_subset_nyc_100k.csv")
 
 taxi_data_sf<-st_as_sf(taxi_data_select,coords=c("pickup_longitude","pickup_latitude"),crs=4326)
 
 st_write(taxi_data_sf,"taxi_data_subset_nyc_ratios_100k_shp.shp")
-
-#Make data long
-taxi_data_drop<-taxi_data_select %>%
-  select(recordID,dropoff_latitude,dropoff_longitude) %>%
-  mutate(type="dropoff") %>%
-  rename("Y"=dropoff_latitude,
-         "X"=dropoff_longitude)
-
-taxi_data_pickup<-taxi_data_select %>%
-  select(recordID,pickup_latitude,pickup_longitude) %>%
-  mutate(type="pickup") %>%
-  rename("Y"=pickup_latitude,
-         "X"=pickup_longitude)
-
-taxi_data_long<-rbind(taxi_data_pickup,taxi_data_drop)
-
-write_csv(taxi_data_long,"taxi_data_nyc_long_1k.csv")
-
-taxi_data_long_sf<-st_as_sf(taxi_data_long,coords=c("X","Y"),crs=4326)
-
-st_write(taxi_data_long_sf,"taxi_data_nyx_long_1k_shp.shp")
